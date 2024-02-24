0fb0281cb90e9c0775f337a7eae90736f699fe8a
Veronika Kiss <kissv@flip4.engr.oregonstate.edu>
2024-02-23 20:19:20 -0800

    add file structure and create app.py, main.j2

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template
+import os
+
+# Configuration
+
+app = Flask(__name__)
+
+# Create people dictionary
+people_from_app_py = [
+{
+    "name": "Thomas",
+    "age": 33,
+    "location": "New Mexico",
+    "favorite_color": "Blue"
+},
+{
+    "name": "Gregory",
+    "age": 41,
+    "location": "Texas",
+    "favorite_color": "Red"
+},
+{
+    "name": "Vincent",
+    "age": 27,
+    "location": "Ohio",
+    "favorite_color": "Green"
+},
+{
+    "name": "Alexander",
+    "age": 29,
+    "location": "Florida",
+    "favorite_color": "Orange"
+}
+]
+
+# Routes 
+
+@app.route('/')
+def root():
+    return render_template("main.j2", people=people_from_app_py)
+
+# Listener
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 4925)) 
+    #                                 ^^^^
+    #              You can replace this number with any valid port
+    
+    app.run(port=port, debug=True) 
+
