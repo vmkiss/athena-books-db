b1a4eb8d76e6e03a5bc752734e33cd77b1524b8d
Veronika Kiss <kissv@flip4.engr.oregonstate.edu>
2024-02-25 20:04:18 -0800

    connect app to database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
 import os
 import database.db_connector as db
 db_connection = db.connect_to_database()
@@ -6,47 +6,27 @@ db_connection = db.connect_to_database()
 # Configuration
 
 app = Flask(__name__)
-
-# Create people dictionary
-people_from_app_py = [
-{
-    "name": "Thomas",
-    "age": 33,
-    "location": "New Mexico",
-    "favorite_color": "Blue"
-},
-{
-    "name": "Gregory",
-    "age": 41,
-    "location": "Texas",
-    "favorite_color": "Red"
-},
-{
-    "name": "Vincent",
-    "age": 27,
-    "location": "Ohio",
-    "favorite_color": "Green"
-},
-{
-    "name": "Alexander",
-    "age": 29,
-    "location": "Florida",
-    "favorite_color": "Orange"
-}
-]
+db_connection = db.connect_to_database()
 
 # Routes 
 
 @app.route('/')
 def root():
-    return render_template("main.j2", people=people_from_app_py)
+    return render_template("main.j2")
+
+@app.route('/bsg-people')
+def bsg_people():
+    query = "SELECT * FROM bsg_people;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+
+    return render_template("bsg.j2", bsg_people=results)
 
 # Listener
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 4925)) 
     #                                 ^^^^
-    #              You can replace this number with any valid port
     
     app.run(port=port, debug=True) 
 
