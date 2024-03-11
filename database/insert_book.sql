e89cb87ad7cd5dec3a6cbb1a92e660b7c6574784
Veronika Kiss <kissv@flip3.engr.oregonstate.edu>
2024-03-11 12:49:50 -0700

    delete test sql file

--- database/insert_book.sql
+++
@@ -1,5 +0,0 @@
-SELECT Books.bookID as BookID, Books.title AS Title, Authors.authorName AS Author, Publishers.publisherName as Publisher, Books.genre AS Genre, Books.price as Price, Books.inventoryQty as Quantity 
-FROM Books 
-INNER JOIN Authors ON Authors.authorID = Books.authorID 
-INNER JOIN Publishers ON Publishers.publisherID = Authors.publisherID 
-ORDER BY Books.title;
