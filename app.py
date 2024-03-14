902411e02ac31208296526faf50a3eefe3ff3a23
Veronika Kiss <kissv@flip1.engr.oregonstate.edu>
2024-03-13 22:42:56 -0700

    adjust browse table's top margin

--- app.py
+++ app.py
@@ -549,7 +549,7 @@ def edit_bookpurchases(BookPurchasesID):
 
 if __name__ == "__main__":
 
-    port = int(os.environ.get('PORT', 4928)) 
+    port = int(os.environ.get('PORT', 4929)) 
      
     app.run(port=port, debug=True) 
 
