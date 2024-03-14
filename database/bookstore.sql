8f587cda1f70caebcb04c590dd6da8fa95a000c8
Veronika Kiss <kissv@flip3.engr.oregonstate.edu>
2024-03-14 16:35:15 -0700

    remove publisherID from Books entity insert statement

--- database/bookstore.sql
+++ database/bookstore.sql
@@ -116,13 +116,11 @@ VALUES ('Harper Lee', (SELECT publisherID from Publishers WHERE publisherName = 
 ;
 
 -- inserts sample data into Books table
-INSERT INTO Books (publisherID, authorID, title, genre, price, inventoryQty)
-VALUES ((SELECT publisherID FROM Publishers WHERE publisherName = 'HarperCollins'),
-(SELECT authorID FROM Authors WHERE authorName = 'Harper Lee'), 'To Kill a Mockingbird', 'literary', 16.99, 105),
-((SELECT publisherID FROM Publishers WHERE publisherName = 'Simon & Schuster'),
-(SELECT authorID FROM Authors WHERE authorName = 'P.G. Wodehouse'), 'Right Ho, Jeeves', 'humor', 19.99, 14),
-((SELECT publisherID from Publishers WHERE publisherName = 'Penguin Random House'), (SELECT authorID FROM Authors WHERE authorName = 'Terry Pratchett'), 'Monstrous Regiment', 'fantasy', 15.98, 25),
-((SELECT publisherID from Publishers WHERE publisherName = 'Penguin Random House'), (SELECT authorID FROM Authors WHERE authorName = 'Louisa May Alcott'), 'Little Women', 'literary', 17.99, 36)
+INSERT INTO Books (authorID, title, genre, price, inventoryQty)
+VALUES ((SELECT authorID FROM Authors WHERE authorName = 'Harper Lee'), 'To Kill a Mockingbird', 'literary', 16.99, 105),
+((SELECT authorID FROM Authors WHERE authorName = 'P.G. Wodehouse'), 'Right Ho, Jeeves', 'humor', 19.99, 14),
+((SELECT authorID FROM Authors WHERE authorName = 'Terry Pratchett'), 'Monstrous Regiment', 'fantasy', 15.98, 25),
+((SELECT authorID FROM Authors WHERE authorName = 'Louisa May Alcott'), 'Little Women', 'literary', 17.99, 36)
 ;
 
 -- inserts sample data into Purchases table
